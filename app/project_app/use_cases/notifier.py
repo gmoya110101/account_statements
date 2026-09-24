e68a8b151f56cb6ac4fc90d5d2d2e222a8e31991
gmoya110101 <giturribarriam@gmail.com>
2026-09-24 17:07:53 -0600

    feat: add notifier use case; rename use case executor to account_statement;

--- app/project_app/use_cases/notifier.py
+++ app/project_app/use_cases/notifier.py
@@ -1,25 +1,30 @@
-"""Executor use case for processing jobs."""
+import argparse
+
+from dotenv import load_dotenv
 
 from app.project_app.app_settings import settings
-from app.project_app.repositories.processors.executor.job import ExecutorJob
+from app.project_app.repositories.processors.executors.notifier.job import (
+    NotifierJob,
+)
 
+load_dotenv()
+
+parser = argparse.ArgumentParser(
+    description="Executes Notifier job.",
+)
 
 def main() -> None:
     """Main function for the executor use case."""
-    grafana_url = settings.grafana.URL
-    grafana_token = settings.grafana.TOKEN
 
     telegram_base_url = settings.telegram.URL
     telegram_endpoint = settings.telegram.ENDPOINT
     telegram_token = settings.telegram.TOKEN
     telegram_message= settings.telegram.MESSAGE
 
-    job = ExecutorJob(
+    debtors = settings.global_vars.DEBTORS
+
+    job = NotifierJob(
         settings={
-            "downloader": {
-                "url": grafana_url,
-                "token": grafana_token,
-            },
             "sender": {
                 "base_url": telegram_base_url,
                 "endpoint": telegram_endpoint,
@@ -28,6 +33,8 @@ def main() -> None:
             },
         }
     )
-    job.execute(debtors=[
-                {"name": "Gerardo", "chat_id": "6113056518"},
-            ])
+    job.execute(debtors=debtors)
+
+if __name__ == "__main__":
+    """Execute the notifier job."""
+    main()
